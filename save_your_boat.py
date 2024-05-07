058c07c621180c44be835db039607750635f4566
Raghadkatout08 <raghadkatout67>
2024-05-07 18:18:03 +0000

    done

--- save_your_boat.py
+++ save_your_boat.py
@@ -1,35 +1,43 @@
 import random
 
-desc1 = "\t\t1) You have a boat that have 5 lives end it will drwn"
-desc2 = "\t\t2) You should guess the word by choosing a letter from the English alphabet"
-desc3 = "\t\t3) If you got a letter wrong you will lose a live"
-desc5 = "\t\t4) If you guess everything right you will win"
-desc4 = "\t\t5) When you guess the letter right you will be provided with the update word"
-desc6 = "\t\t6) If you finish your lives and did not guess the word you will lose"
-desc7 = "\t\t7) To quit you can write exit"
+def welcome_message():
+    
+    welcoming_message= "Welcome To Save Boat Game :D"
+    desc = "Description: "
+    desc1 = "1) You have a boat that have 5 lives end it will drwn"
+    desc2 = "2) You should guess the word by choosing a letter from the English alphabet"
+    desc3 = "3) If you got a letter wrong you will lose a live"
+    desc5 = "4) If you guess everything right you will win"
+    desc4 = "5) When you guess the letter right you will be provided with the update word"
+    desc6 = "6) If you finish your lives and did not guess the word you will lose"
+    desc7 = "7) To quit you can write exit"
 
-print("\tWelcome To Save Boat Game :D \n\n\tDescription :\n\n {}\n {}\n {}\n {}\n {}\n {}\n {}".format(desc1,desc2, desc3, desc4, desc5, desc6, desc7)) 
+    print("\t{} \n\n\t{} \n \n \t\t{} \n\t\t{} \n\t\t{} \n\t\t{} \n\t\t{} \n\t\t{} \n\t\t{} \n".format(welcoming_message,desc,desc1,desc2,desc3,desc4,desc5,desc6,desc7)) 
 
-answer = input("\n\nWanna play ? (y,n) : ")
+welcome_message()
 
-hints = ["apple" , "orange" , "banana" ,  "lemon" , "mango"]
+question = input("Wanna Play ? (y,n) ? ")
 
-def give_hint(hints_given):
-    hints = ["food", "maybe red, green, yellow"]
-    if hints_given == 0:
-        print("Hint:", hints[0])
-    elif hints_given == 1:
-        print("Hint:", hints[1])
-    elif hints_given == 2:
-        print("Hint: It starts with the letter 'a'")
+fruit_hint = ['apple' , 'melon' , 'mango' , 'olive', 'lemon']
 
-def save_your_boat():
-    word_to_guess = random.choice(hints)
+
+def give_hint(lives):
+    hints = ["  - food", "  - maybe red, green, yellow"]
+
+    
+    
+    print("guess this: \n _ _ _ _ _")
+    print("\t\t Hint: \n\t\t", hints[0] , "\n\t\t" , hints[1])
+
+def guess_letter():
+    word_to_guess = random.choice(fruit_hint)
     word_display = "_" * len(word_to_guess)
     lives = 5
     hints_given = 0
 
     print("Let's play the game!\n")
+
+    give_hint(lives)
 
     while lives > 0:
         print("Word to guess:", " ".join(word_display))
@@ -41,7 +49,7 @@ def save_your_boat():
             print("Exiting the game.")
             break
 
-        if len(guess) != 1 or not guess.isalpha():
+        if len(guess) != 1:
             print("Please enter a single letter.")
             continue
 
@@ -54,11 +62,14 @@ def save_your_boat():
                 else:
                     updated_word_display += word_display[i]
             word_display = updated_word_display
+
         else:
-            lives -= 1
             print("You got it wrong.")
-            give_hint(hints_given)
-            hints_given += 1
+            lives -= 1
+            if lives == 1:
+                print("Here's a hint:")
+                print("  - Hint: It starts with the letter '{}'".format(word_to_guess[0]))
+
             if lives == 0:
                 print("Game Over! You ran out of lives. The word was:", word_to_guess)
                 break
@@ -70,7 +81,7 @@ def save_your_boat():
 
 
 if __name__ == "__main__":
-    if answer.lower() == 'y':
-        save_your_boat()
+    if question.lower() == 'y':
+        guess_letter()
     else:
         print("Maybe next time! Bye!")
